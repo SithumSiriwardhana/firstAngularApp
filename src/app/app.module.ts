e746ce4db836c2342a47628ce2295f4f63540bc8
Sithum Siriwardhana <sithumdharshana22@gmail.com>
2023-02-07 22:07:57 +0530

    2/7/2023 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 
@@ -11,8 +10,7 @@ import { ProductsComponent } from './components/products/products.component';
     ProductsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
