c885357bbc1abc779c5335d97cceaf2cf686904f
Sithum Siriwardhana <sithumdharshana22@gmail.com>
2023-02-08 20:12:07 +0530

    Angular directive lession

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,11 +7,55 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
-  public firstProductItemName = "White Basmathi Rice";
+  // public firstProductItemName = "White Basmathi Rice";
 
-  public getPriceofRice() {
-    return 350;
-  }
+  // public getPriceofRice() {
+  //   return 350;
+  // }
+
+  // public isLowInventory = false;
+  // public dhalStorage = 10;
+
+  // public getDhalStorage() {
+  //   if (this.dhalStorage < 50)
+  //     this.isLowInventory = true;
+  //   return this.dhalStorage
+  // }
+
+  public products = [
+    {
+      'productId': "001",
+      'productName': "White Basmathi Rice",
+      'createdDate': "Jan 29, 2023",
+      'unitPrice': "Rs. 400",
+      'quantity': 100,
+      'productDescription': "White Basmathi Rice imported from pakistan"
+    },
+    {
+      'productId': "002",
+      'productName': "Flour",
+      'createdDate': "Jan 28, 2023",
+      'unitPrice': "Rs. 200",
+      'quantity': 50,
+      'productDescription': "Super fine Whole grain general Purpose flour"
+    },
+    {
+      'productId': "003",
+      'productName': "Sugar",
+      'createdDate': "Jan 5, 2023",
+      'unitPrice': "Rs. 300",
+      'quantity': 80,
+      'productDescription': "White sugar manufactured by Palwatte Factory"
+    },
+    {
+      'productId': "004",
+      'productName': "Dhal",
+      'createdDate': "Jan 26, 2023",
+      'unitPrice': "Rs. 250",
+      'quantity': 20,
+      'productDescription': "Imported mysoor dhal from India"
+    }
+  ]
 
 
 }
