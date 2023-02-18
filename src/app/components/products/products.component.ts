6fb6fe8ced06b2598d6bc8aca581134891750e1b
Sithum Siriwardhana <sithumdharshana22@gmail.com>
2023-02-18 23:52:00 +0530

    Data Binding and Pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -22,12 +22,41 @@ export class ProductsComponent {
   //   return this.dhalStorage
   // }
 
+  public rowIndex: number = -1;
+  public isRowSelected: boolean = false;
+  
+  public selectProduct(selectedRow: number) {
+
+      if (this.isRowSelected && this.rowIndex == selectedRow && this.clickingButton == true) {
+        this.isRowSelected = true;
+        this.rowIndex = selectedRow;
+      }
+      else if (this.isRowSelected && this.rowIndex == selectedRow) {
+        this.isRowSelected = false;
+        this.rowIndex = -1;
+      }
+      else {
+        this.isRowSelected = true;
+        this.rowIndex = selectedRow;
+    }
+    this.clickingButton = false;
+  }
+ 
+
+  public clickingButton: boolean = false;
+
+  public buttonClick() {
+
+    return this.clickingButton = true;
+  }
+
+
   public products = [
     {
       'productId': "001",
       'productName': "White Basmathi Rice",
-      'createdDate': "Jan 29, 2023",
-      'unitPrice': "Rs. 400",
+      'createdDate': "2023.1.1",
+      'unitPrice': "400",
       'quantity': 100,
       'productDescription': "White Basmathi Rice imported from pakistan"
     },
@@ -35,7 +64,7 @@ export class ProductsComponent {
       'productId': "002",
       'productName': "Flour",
       'createdDate': "Jan 28, 2023",
-      'unitPrice': "Rs. 200",
+      'unitPrice': "20",
       'quantity': 50,
       'productDescription': "Super fine Whole grain general Purpose flour"
     },
@@ -43,7 +72,7 @@ export class ProductsComponent {
       'productId': "003",
       'productName': "Sugar",
       'createdDate': "Jan 5, 2023",
-      'unitPrice': "Rs. 300",
+      'unitPrice': "300",
       'quantity': 80,
       'productDescription': "White sugar manufactured by Palwatte Factory"
     },
@@ -51,11 +80,9 @@ export class ProductsComponent {
       'productId': "004",
       'productName': "Dhal",
       'createdDate': "Jan 26, 2023",
-      'unitPrice': "Rs. 250",
+      'unitPrice': "250",
       'quantity': 20,
       'productDescription': "Imported mysoor dhal from India"
     }
   ]
-
-
 }
